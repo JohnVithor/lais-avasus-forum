57ef4051d8c5b3f41e83aab4ede90121ce2fc837
JohnVithor <jv.venceslau.c@gmail.com>
2022-12-23 15:03:16 -0300

    adiciona informações iniciais do forum

--- forum/models.py
+++ forum/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField('Nome', max_length=100, help_text='Informe o nome da categoria')
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField('Criado em', auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.name
+
